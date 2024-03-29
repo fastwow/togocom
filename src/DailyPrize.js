5922caceca799490d547e03baa3c33c08dd6d01a
Artem Dudinskyi <artem.dudynskiy@gmail.com>
2024-03-29 16:49:14 +0200

    Updates

--- src/DailyPrize.js
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import DrawContainer from "./DrawContainer";
-
-const DailyPrize = () => {
-  return <DrawContainer type="daily" date="29/03/2024" />;
-};
-
-export default DailyPrize;
