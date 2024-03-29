1e695a53227e045f23ed18849f0b69c96688b6a8
Artem Dudinskyi <artem.dudynskiy@gmail.com>
2024-03-29 14:14:51 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import DailyPrize from "./DailyPrize";
+import SuperPrize from "./SuperPrize";
+import WeeklyPrize from "./WeeklyPrize";
 
 const App = () => {
-  return <DailyPrize />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/weekly" element={<WeeklyPrize />} />
+        <Route path="/super" element={<SuperPrize />} />
+        <Route path="/" element={<DailyPrize />} />
+        <Route path="*" element={<DailyPrize />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
