5d0664e89d00197b4a172602730e3636c2a4cbb0
Artem Dudinskyi <artem.dudynskiy@gmail.com>
2024-04-02 14:26:39 +0300

    update

--- src/firebaseApi.js
+++ src/firebaseApi.js
@@ -1,7 +1,7 @@
 import { DATA } from "./mocks";
 import { db } from "./firebase";
 import { collection, addDoc, query, getDocs } from "firebase/firestore";
-import firebase from "firebase/compat/app";
+import { Timestamp } from "firebase/firestore";
 
 const getDataByData = async (date, type) => {
   console.log("date", date);
@@ -34,29 +34,20 @@ const getAllData = async () => {
   return data;
 };
 
-// Assuming dateStr is in the format DD-MM-YYYY
-function convertToDateTimestamp(dateStr) {
-  // Split the date string into day, month, and year
-  const parts = dateStr.split("-");
-  const day = parseInt(parts[0], 10);
-  const month = parseInt(parts[1], 10) - 1; // Month in JavaScript starts from 0
-  const year = parseInt(parts[2], 10);
-
-  // Create a JavaScript Date object
-  const date = new Date(year, month, day);
-
-  // Convert the Date object to Firestore Timestamp
-  const timestamp = firebase.firestore.Timestamp.fromDate(date);
-
-  return timestamp;
-}
-
 const createItem = async (newItem) => {
   console.log("newItem", newItem);
 
   // convert newItem.date from string in format DD-MM-YYYY to firebase timestamp
 
-  // newItem.date = convertToDateTimestamp(newItem.date);
+  // Convert your date to a JavaScript Date object
+  const dateParts = newItem.date.split("/");
+  const jsDate = new Date(dateParts[2], dateParts[1] - 1, dateParts[0]); // month is zero-based
+
+  console.log("jsDate", jsDate);
+
+  // Then convert it to a Firebase Timestamp
+  const timestamp = Timestamp.fromDate(jsDate);
+  newItem.date = timestamp;
 
   console.log("newItem", newItem);
 
